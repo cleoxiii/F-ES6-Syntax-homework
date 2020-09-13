7423f15e525dccd6cd05fc946007f731a6b72f81
Yun Chenxi <cleoxiii@163.com>
2020-09-14 00:30:17 +0800

    [yunchenxi] feat: finish practice 1 & practice 2.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (input) => {
+  const result = [];
+  for (const dataItem of input.data) {
+    let expectObject = {};
+    let index = 0;
+    for (const dataDescription of input.column) {
+      expectObject[dataDescription.name] = dataItem[index];
+      index++;
+    }
+    result.push(expectObject);
+  }
+
+  return result;
+}
 export { parseData };
