234f28641111f4f1985419817dd917ae74f11717
kevindongzg <dongkevin94@gmail.com>
2020-09-22 20:17:46 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,11 @@
+// TODO 可以思考一下可否用reduce来做
+// TODO 使用解构的方式取出data与column
 const parseData = (input) => {
+  // TODO 使用map做数组与数组的转化，不用声明新数组
   const result = [];
   for (const dataItem of input.data) {
     let expectObject = {};
+    // TODO 不用重新声明 index，使用valueList.entries或forEach
     let index = 0;
     for (const dataDescription of input.column) {
       expectObject[dataDescription.name] = dataItem[index];
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+  // TODO 不是map的使用场景
   sections.map((section, index) => {
     items.splice(section.index + index, 0, section.content)
   })
